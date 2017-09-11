a9c343b2487dd1d839e183b2408e072fa4fc178f
Daniil Blagiy <blagiy.daniil@gmail.com>
2017-09-11 19:23:25 +0300

    working on main frame buttons handling

--- src/com/znow/financemanaging/controllers/Controller.java
+++ src/com/znow/financemanaging/controllers/Controller.java
@@ -2,6 +2,24 @@ package com.znow.financemanaging.controllers;
 
 public class Controller {
 	
+	public void onIncomeCatButton() {
+		
+	}
 	
+	public void onExpenseCatButton() {
+		
+	}
+	
+	public void onReportsButton() {
+		
+	}
+	
+	public void onAddIncomeButton() {
+		
+	}
+	
+	public void onAddExpenseButton() {
+		
+	}
 	
 }
--- src/com/znow/financemanaging/gui/MainFrame.java
+++ src/com/znow/financemanaging/gui/MainFrame.java
@@ -1,11 +1,18 @@
 package com.znow.financemanaging.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import com.znow.financemanaging.controllers.Controller;
+
 class MainFrame extends JPanel {
+	
+	private Controller controller = new Controller();
 	
 	MainFrame() {
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
@@ -16,20 +23,50 @@ class MainFrame extends JPanel {
 		
 		JPanel categoriesButtonsPane = new JPanel();
 		JButton incomeCatButton = new JButton("Income categories");
+		incomeCatButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("income cat");
+			}
+		});
 		categoriesButtonsPane.add(incomeCatButton);
 		JButton expenseCatButton = new JButton("Expense categories");
+		expenseCatButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("expense cat");
+			}
+		});
 		categoriesButtonsPane.add(expenseCatButton);
 		categoriesButtonsPane.setAlignmentX(JPanel.CENTER_ALIGNMENT);
 		add(categoriesButtonsPane);
 		
 		JButton reportsButton = new JButton("See income/expense reports");
+		reportsButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("see reports");
+			}
+		});
 		reportsButton.setAlignmentX(JPanel.CENTER_ALIGNMENT);
 		add(reportsButton);
 		
 		JPanel addButtonsPane = new JPanel();
 		JButton addIncomeButton = new JButton("Add income");
+		addIncomeButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("add income");
+			}
+		});
 		addButtonsPane.add(addIncomeButton);
 		JButton addExpenseButton = new JButton("Add Expense");
+		addExpenseButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("add expense");
+			}
+		});
 		addButtonsPane.add(addExpenseButton);
 		addButtonsPane.setAlignmentX(JPanel.CENTER_ALIGNMENT);
 		add(addButtonsPane);
